810185e38d514692fae191d8037931687698a41c
Rubem Kalebe <rubemkalebe@hotmail.com>
2015-03-04 14:30:24 -0300

    Nova classe casa

---
+++ Paranatalândia/src/Casa.java
@@ -0,0 +1,17 @@
+public class Casa {
+    
+    private int id;
+
+    public Casa(int id) {
+        this.id = id;
+    }
+    
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+    
+}
