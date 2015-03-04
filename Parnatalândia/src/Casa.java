cc2532c2bfba9d1666208714a5da9570d30281c1
Rubem Kalebe <rubemkalebe@hotmail.com>
2015-03-04 15:10:12 -0300

    Renomeação de pasta; Alteração na classe Casa.

---
+++ Parnatalândia/src/Casa.java
@@ -0,0 +1,25 @@
+/**
+ * Essa classe é responsável por representar uma casa no cenário.
+ * 
+ * @author Lilian Ketlyn
+ * @author Rubem Kalebe
+ * @version 04.03.2015
+ */
+
+public class Casa {
+    
+    private int id;
+
+    public Casa(int id) {
+        this.id = id;
+    }
+    
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+}
